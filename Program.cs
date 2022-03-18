e96dcf53905f5391f547b56a2ebbb041e8f6cedd
VANDO MONTEIRO <vando.monteiro@etec.sp.gov.br>
2022-03-18 10:55:13 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1000_media_simples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Calcular a média aritimética simples.");
+            Console.WriteLine();
+
+            Console.Write("Digite o valor 1: ");
+            float a = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor 2: ");
+            float b = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor 3: ");
+            float c = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Valor da média  : "+(a + b + c) / 3.0f);
+            
+
+            Console.ReadLine();
+        }
+    }
+}
